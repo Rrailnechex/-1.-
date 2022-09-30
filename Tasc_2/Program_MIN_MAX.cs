8844f538197b327b78545e3da26b142e41a58237
Ygor <wanerosu8@gmail.com .com>
2022-09-30 21:33:56 +0300

    initial commit

---
+++ Tasc_2/Program_MIN_MAX.cs
@@ -0,0 +1,32 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+int MAX = 0;
+int MIN = 0;
+
+
+Console.WriteLine("Введите число А");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+int B = int.Parse(Console.ReadLine());
+
+
+if (A > B)
+{
+    MAX = A;
+}
+else
+{
+    MIN = B;
+}
+
+
+Console.Write("MAX = ");
+Console.WriteLine(A);
+
+Console.Write("MIN = ");
+Console.WriteLine(B);
---
+++ Task_8/Program_x/2_in_N_cycle.cs
@@ -0,0 +1,20 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Число N = ");
+int The_N_Number = int.Parse(Console.ReadLine());
+
+int Temp_Num = 0;
+
+
+Console.Write("Result = ");
+while (Temp_Num <= The_N_Number)
+{
+    if (Temp_Num != 0) { if (Temp_Num % 2 == 0) { Console.Write(Temp_Num); Console.Write(", "); } }
+    Temp_Num += 1;
+}
+
+
+
